9e03d4d40f05e31115d4a1c438d8133fa78e232d
Andre Gomez <54462949+gomez1983@users.noreply.github.com>
2024-07-09 20:14:43 -0300

    Conectando com banco de dados

--- main.go
+++ main.go
@@ -1,10 +1,22 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 	"text/template"
+
+	_ "github.com/lib/pq" /*O underline serve para manter o import mesmo se ele não for utilizado*/
 )
+
+func conectaComBancoDeDados() *sql.DB {
+	conexao := "user=postgres dbname=alura_loja password=catita01 host=localhost sslmode=disable"
+	db, err := sql.Open("postgres", conexao)
+	if err != nil {
+		panic(err.Error())
+	}
+	return db
+}
 
 type Produto struct {
 	Nome       string
@@ -16,6 +28,8 @@ type Produto struct {
 var temp = template.Must(template.ParseGlob("templates/*.html")) /* Encapsula todos os templates html do pacote para a variável*/
 
 func main() {
+	db := conectaComBancoDeDados()
+	defer db.Close()
 	http.HandleFunc("/", index)
 	http.ListenAndServe(":8000", nil)
 }
