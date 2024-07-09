7770577c5b0a15ed14211daad9d52850593a4e3a
Andre Gomez <54462949+gomez1983@users.noreply.github.com>
2024-07-09 19:19:44 -0300

    Criada struct de produtos

--- main.go
+++ main.go
@@ -1,19 +1,34 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	"text/template"
 )
 
+type Produto struct {
+	Nome       string
+	Descricao  string
+	Preco      float64
+	Quantidade int
+}
+
 var temp = template.Must(template.ParseGlob("templates/*.html")) /* Encapsula todos os templates html do pacote para a variável*/
 
 func main() {
-
 	http.HandleFunc("/", index)
 	http.ListenAndServe(":8000", nil)
-
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	temp.ExecuteTemplate(w, "Index", nil)
+	produtos := []Produto{
+		{Nome: "Camiseta", Descricao: "Azul, bem bonita", Preco: 39, Quantidade: 5},
+		{"Tenis", "Confortável", 89, 3},
+		{"Fone", "Muito bom", 59, 2},
+		{"Produto novo", "Muito legal", 1.99, 1},
+	}
+	err := temp.ExecuteTemplate(w, "Index", produtos)
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
