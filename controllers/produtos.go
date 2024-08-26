558d3c2f5b692d65c2921923bd38df3ea13c2224
Andre Gomez <54462949+gomez1983@users.noreply.github.com>
2024-08-25 21:12:44 -0300

    Criada função de atualizar produtos

--- controllers/produtos.go
+++ controllers/produtos.go
@@ -44,5 +44,39 @@ func Insert(w http.ResponseWriter, r *http.Request) {
 func Delete(w http.ResponseWriter, r *http.Request) {
 	idDoProduto := r.URL.Query().Get("id") /*A função pega a URL do botão DELETE*/
 	models.DeletaProduto(idDoProduto)      /*Deleta o produto*/
-	http.Redirect(w, r, "/", 301)          /*W é o responseWriter, r é a requisição, '/' é a página principal e 301 é o código do retorno */
+	http.Redirect(w, r, "/", 301)          /*w é o responseWriter, r é a requisição, '/' é a página principal e 301 é o código do retorno */
+}
+
+func Edit(w http.ResponseWriter, r *http.Request) {
+	idDoProduto := r.URL.Query().Get("id")
+	produto := models.EditaProduto(idDoProduto)
+	temp.ExecuteTemplate(w, "Edit", produto)
+}
+
+func Update(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		id := r.FormValue("id")
+		nome := r.FormValue("nome")
+		descricao := r.FormValue("descricao")
+		preco := r.FormValue("preco")
+		quantidade := r.FormValue("quantidade")
+
+		idConvertidaParaInt, err := strconv.Atoi(id)
+		if err != nil {
+			log.Println("Erro na convesão do ID para int:", err)
+		}
+
+		precoConvertidoParaFloat, err := strconv.ParseFloat(preco, 64)
+		if err != nil {
+			log.Println("Erro na convesão do preço para float64:", err)
+		}
+
+		quantidadeConvertidaParaInt, err := strconv.Atoi(quantidade)
+		if err != nil {
+			log.Println("Erro na convesão da quantidade para int:", err)
+		}
+
+		models.AtualizaProduto(idConvertidaParaInt, nome, descricao, precoConvertidoParaFloat, quantidadeConvertidaParaInt)
+	}
+	http.Redirect(w, r, "/", 301)
 }
--- routes/routes.go
+++ routes/routes.go
@@ -10,4 +10,6 @@ func CarregaRotas() {
 	http.HandleFunc("/new", controllers.New)       /*Função com a rota que especifica que vai atender. Aqui indica o caminho da página "New"*/
 	http.HandleFunc("/insert", controllers.Insert) /*Função com a rota que especifica que vai atender. Aqui indica o caminho quando tiver requisição para Insert*/
 	http.HandleFunc("/delete", controllers.Delete) /*Função com a rota que especifica que vai atender. Aqui indica o caminho quando tiver requisição para Deletar*/
+	http.HandleFunc("/edit", controllers.Edit)     /*Função com a rota que especifica que vai atender. Aqui indica o caminho quando tiver requisição para Editar*/
+	http.HandleFunc("/update", controllers.Update) /*Função com a rota que especifica que vai atender. Aqui indica o caminho quando tiver requisição para Atualizar o que for editado*/
 }
